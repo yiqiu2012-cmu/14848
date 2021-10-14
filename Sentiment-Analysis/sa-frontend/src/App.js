cb90dbc3e78a1d908805d18b83f34b08eb504ffc
Yi Qiu <yiqiu@andrew.cmu.edu>
2021-10-14 00:19:43 -0400

    extra-mini

--- Sentiment-Analysis/sa-frontend/src/App.js
+++ Sentiment-Analysis/sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://34.70.191.201:8080/sentiment', {
+        fetch('http://localhost:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
